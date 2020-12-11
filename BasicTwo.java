4ff27b35505d9179f0f1dd06c5da67363e41f561
venkat sai reddy <yarrapureddyvenkat@gmail.com>
2020-12-11 16:17:17 +0530

    commites

---
+++ BasicTwo.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+class BasicTwo
+{
+	public static void main(String[] args) 
+	{
+			int n;
+Scanner sc=new Scanner(System.in);
+		System.out.println("entre the vusre value");
+		n=sc.nextInt();
+			if(n%2==0)
+		{
+		System.out.println("Hello World!");
+		}
+		else
+		{
+		System.out.println("me"+-(n));
+		}
+
+	}
+}
